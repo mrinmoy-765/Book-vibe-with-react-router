3c165ccc1955a5fe31c92f5204a4f16b3843fbfc
mrinmoy-765 <mrinmoyd629@gmail.com>
2025-02-16 19:41:45 +0600

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://wallpapercave.com/wp/wp8442906.jpg"
+          class="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-6xl font-bold text-purple-400">book shop</h1>
+          <p className="py-6 text-2xl font-extrabold text-gray-500">
+            The power to be found between the pages of a book is formidable,
+            indeed.
+          </p>
+          <span className="text-gray-800">
+            "It is really hard to be lonely very long in a world of words. Even
+            if you don't have friends somewhere, you still have language, and it
+            will find you and wrap its little syllables around you and suddenly
+            there will be a story to live in." – Naomi Shihab Nye, I'll Ask You
+            Three Times, Are You OK?: Tales of Driving and Being Driven
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <h2 className="text-5xl text-red-600">Page Not Found</h2>
+    </div>
+  )
+}
+
+export default ErrorPage
+
